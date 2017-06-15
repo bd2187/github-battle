18d4b468b3f59066a6e3c8605cf95c6a6c11a3cc
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-06-15 13:25:34 -0700

    Add utils directory and api.js. Install axios module. Make api request to github api.

---
+++ app/utils/api.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import axios from 'axios';
+
+export default {
+  fetchPopularRepos(language) {
+    var encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language: ${language}&sort=stars&order=desc&type=Repositories`);
+  }
+
+  return axios.get(encodedURI)
+    .then(function(res){
+      return res.data.items;
+    })
+    .catch(function(err){
+      return err;
+    });
+}
