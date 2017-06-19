f3263df0f0f6e79712bcfcde0b7eadcbaa54530f
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-06-19 11:40:37 -0700

    Utilize props.children in PlayerPreview component since we are reusing the component in both the Battle and Results components. Each component will render different props.children.

--- app/components/PlayerPreview.js
+++ app/components/PlayerPreview.js
@@ -7,28 +7,21 @@ function PlayerPreview(props) {
   return (
     <div>
       <div className="column">
-      <img
-        className="avatar"
-        src={props.avatar}
-        alt={`Avatar for ${props.username}`}
-      />
-      <h2 className="username">@{props.username}</h2>
-      <button
-        className="reset"
-        onClick={props.onReset.bind(null, props.id)}
-      >
-        Reset
-      </button>
+        <img
+          className="avatar"
+          src={props.avatar}
+          alt={`Avatar for ${props.username}`}
+        />
+        <h2 className="username">@{props.username}</h2>
       </div>
+      {props.children}
     </div>
   )
 }
 
 PlayerPreview.propTypes = {
   avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired
+  username: PropTypes.string.isRequired
 }
 
 export default PlayerPreview;
